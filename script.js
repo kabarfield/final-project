7e53a68caacb5246ad4340bcad839f3bf3bdc4d9
kabarfield <35902515+kabarfield@users.noreply.github.com>
2018-04-24 19:36:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+var elem = document.querySelector('.parallax');
+var instance = M.Parallax.init(elem, options);
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
